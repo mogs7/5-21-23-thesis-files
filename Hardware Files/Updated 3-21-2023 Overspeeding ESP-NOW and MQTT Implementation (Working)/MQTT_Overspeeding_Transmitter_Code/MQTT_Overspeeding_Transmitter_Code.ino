0223ff54d35af570714437c47a8344358c7de821
mogs7 <70359414+mogs7@users.noreply.github.com>
2023-06-04 20:13:18 +0800

    Update MQTT_Overspeeding_Transmitter_Code.ino

--- Hardware Files/Updated 3-21-2023 Overspeeding ESP-NOW and MQTT Implementation (Working)/MQTT_Overspeeding_Transmitter_Code/MQTT_Overspeeding_Transmitter_Code.ino
+++ Hardware Files/Updated 3-21-2023 Overspeeding ESP-NOW and MQTT Implementation (Working)/MQTT_Overspeeding_Transmitter_Code/MQTT_Overspeeding_Transmitter_Code.ino
@@ -1,10 +1,8 @@
-// Include Libraries
 #include <esp_now.h>
 #include <WiFi.h>
 
 #define LED 2
 
-// Variables for test data
 unsigned long startTime = 0, timer = 0;
 const float timeOut = 5000.0; // Time out timer in ms
 const float DISTANCE = 13.88, MAX_SPEED = 2.7; //Distance in m and speed in m/s
@@ -15,7 +13,7 @@ bool sensor1Triggered = false, sensor2Triggered = false;
 // Reading to be received from other board
 int pir2Status;
 
-// MAC Address of responder (not in use)
+// Responder MAC Address
 uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0xD0, 0x51, 0xE0}; 
 
 // Stores data to be sent / received
@@ -29,16 +27,13 @@ struct_message sendData;
 // Structured object for receiving
 struct_message receiveData; 
 
-// Store peer info
 esp_now_peer_info_t peerInfo;
 
-// Callback function called when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
   //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-// Callback function executed when data is received 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&receiveData, incomingData, sizeof(receiveData));
   pir2Status = receiveData.sensorStatus;
@@ -46,14 +41,10 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 }
 
 void setup() {
-  
-  // Set up Serial Monitor
   Serial.begin(115200);
- 
-  // Set ESP32 as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Initilize ESP-NOW
+  // Start ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -61,15 +52,12 @@ void setup() {
     Serial.println("ESP-NOW Engaged");
   }
 
-  // Register the send callback
   esp_now_register_send_cb(OnDataSent);
   
-  // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
-  
-  // Add peer        
+    
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Failed to add peer");
     return;
