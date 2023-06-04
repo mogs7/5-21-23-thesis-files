9e9cf55f2391f5337120047a970b7f260fdee67a
mogs7 <70359414+mogs7@users.noreply.github.com>
2023-06-04 20:15:36 +0800

    Update Illegal_Lane_Change_Receiver.ino

--- Hardware Files/Updated 3-23-2023 ILC ESP-NOW and MQTT Implementation (Working)/Illegal_Lane_Change_Receiver/Illegal_Lane_Change_Receiver.ino
+++ Hardware Files/Updated 3-23-2023 ILC ESP-NOW and MQTT Implementation (Working)/Illegal_Lane_Change_Receiver/Illegal_Lane_Change_Receiver.ino
@@ -13,16 +13,13 @@ typedef struct struct_message {
   int sensorStatus;
 } struct_message;
 
-// Structured object for receiving
 struct_message receiveData;
 
-// Callback function called when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
   //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-// Callback function executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&receiveData, incomingData, sizeof(receiveData));
   pir2Status = receiveData.sensorStatus;
@@ -36,7 +33,7 @@ void setup() {
   pinMode(pir1, INPUT);
   pinMode(LED, OUTPUT);
 
-  // Initilize ESP-NOW
+  // Start ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -44,7 +41,6 @@ void setup() {
     Serial.println("ESP-NOW Engaged");
   }
 
-  // Register the send callback
   esp_now_register_send_cb(OnDataSent);
 
   esp_now_register_recv_cb(OnDataRecv);
